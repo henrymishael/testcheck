2335f827f7826b9e459571e2b888e6aab9670194
Henry Mishael <henrymishael@gmail.com>
2023-09-20 00:33:01 +0100

    fixed

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -14,7 +14,7 @@ import { handleSubmit } from '../functions/page';
 import { getTrendingMovies } from '../utils/request';
 import post from '../../../public/images/Poster.png';
 import postsm from '../../../public/images/sm.png';
-import  Navbar  from './src/app/components/navbar.jsx';
+import Navbar from './navbar';
 import { IMG_URL } from '../utils/request';
 
 function Header() {
